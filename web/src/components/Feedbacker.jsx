f9ab2f7531ebb0c022b113c501568392775c89ae
Mathias Herzog <mathu@Mathiass-MBP.fritz.box>
2017-10-17 17:46:04 +0200

    add panel to feedbacker

--- web/src/components/Feedbacker.jsx
+++ web/src/components/Feedbacker.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Grid, PanelGroup, Panel } from 'react-bootstrap';
+import { Row, Col, Grid, PanelGroup, Panel, DropdownButton, MenuItem, Button } from 'react-bootstrap';
 import FeedbackerQuestions from '../containers/FeedbackerQuestions';
 import PanelHeader from '../containers/PanelHeader';
 
@@ -35,6 +35,39 @@ class Feedbacker extends Component {
           <Row>
             <Col xs={6} md={2} />
             <Col xs={6} md={8}>
+              <Panel collapsible expanded>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td ><div align="left">Rolle: &nbsp;</div></td>
+                      <td style={{ paddingLeft: '10px' }}>
+                        <div align="left">
+                          <DropdownButton title="bitte auswählen" id="bg-nested-dropdown">
+                            <MenuItem eventKey="1">Vorgesetzter</MenuItem>
+                            <MenuItem eventKey="2">Kunde</MenuItem>
+                            <MenuItem eventKey="2">Arbeitskollege</MenuItem>
+                          </DropdownButton>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><div align="left">Feedbacknehmer: &nbsp;</div></td>
+                      <td style={{ paddingLeft: '10px' }}><div align="left">Mathias Herzog</div></td>
+                    </tr>
+                    <tr>
+                      <td><div align="left">Laufzeit:</div></td>
+                      <td style={{ paddingLeft: '10px' }}><div align="left">31. Oktober 2017</div></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Panel>
+            </Col>
+            <Col xsHidden md={2} />
+          </Row>
+          <hr />
+          <Row>
+            <Col md={2} />
+            <Col md={8}>
               <PanelGroup activeKey={this.state.activeKey} onSelect={this.handleSelect} accordion>
                 { this.props.contextIds.map(contextId => (
                   <Panel
@@ -48,8 +81,12 @@ class Feedbacker extends Component {
                 }
               </PanelGroup>
             </Col>
-            <Col xsHidden md={2} />
+            <Col md={2} />
           </Row>
+          <Col md={10}>
+            <div align="right"><Button pullRight>Absenden</Button></div>
+          </Col>
+          <Col md={2} />
         </Grid>
       </div>
     );
